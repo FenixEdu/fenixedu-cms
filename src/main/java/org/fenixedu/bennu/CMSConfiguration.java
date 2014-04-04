b985ff5f2d6414923d01e0909e28d6d6f507e822
Miguel Borges <mb.borgez@gmail.com>
2014-04-04 18:35:28 +0100

    i18n resources configuration

--- src/main/java/org/fenixedu/bennu/CMSConfiguration.java
+++ src/main/java/org/fenixedu/bennu/CMSConfiguration.java
@@ -1,5 +1,6 @@
 package org.fenixedu.bennu;
 
+import org.fenixedu.bennu.spring.BennuSpringModule;
 import org.fenixedu.cms.domain.Component;
 import org.fenixedu.cms.domain.ComponentType;
 import org.fenixedu.cms.domain.RegisterSiteTemplate;
@@ -13,10 +14,9 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 
-import pt.ist.fenixframework.DomainObject;
-
 @Configuration
 @ComponentScan("org.fenixedu.cms")
+@BennuSpringModule(basePackages = "org.fenixedu.cms", bundles = { "CmsResources" })
 public class CMSConfiguration implements InitializingBean {
     private static final int FILE_MAX_SIZE_IN_BYTES = 10 * 1024 * 1024;
 
