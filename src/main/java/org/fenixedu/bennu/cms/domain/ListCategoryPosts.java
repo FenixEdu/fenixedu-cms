d7d6820acdeebbbbff0aed972000811ceef1e0c6
Miguel Borges <mb.borgez@gmail.com>
2014-07-17 17:40:23 +0100

    posts now support the requirements of fenix announcements

--- src/main/java/org/fenixedu/bennu/cms/domain/ListCategoryPosts.java
+++ src/main/java/org/fenixedu/bennu/cms/domain/ListCategoryPosts.java
@@ -1,10 +1,6 @@
 package org.fenixedu.bennu.cms.domain;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Optional;
 
 import javax.servlet.http.HttpServletRequest;
@@ -20,8 +16,7 @@ import pt.ist.fenixframework.Atomic.TxMode;
 @ComponentType(type = "listCategoryPosts", name = "List Category Posts", description = "Lists the Posts from a given category")
 public class ListCategoryPosts extends ListCategoryPosts_Base {
 
-    public ListCategoryPosts() {
-    }
+    private static final int POSTS_PER_PAGE = 5;
 
     public ListCategoryPosts(Category category) {
         this.setCategory(category);
@@ -33,64 +28,14 @@ public class ListCategoryPosts extends ListCategoryPosts_Base {
         local.put("category", category);        
         global.put("category", category);
         
-        List<Post> posts = new ArrayList<>();
-
-        for (Post p : category.getPostsSet()) {
-            if (p.getComponentSet().size() == 0) {
-                posts.add(p);
-            }
-        }
-
-        Collections.sort(posts, new Comparator<Post>() {
-
-            @Override
-            public int compare(Post o1, Post o2) {
-                return o1.getCreationDate().compareTo(o2.getCreationDate());
-            }
-        });
-
-        int pages = (posts.size() / ListPosts.POSTS_PER_PAGE) + 1;
-        HashMap<String, Object> pagination = new HashMap<>();
-
-        String currentPageString = req.getParameter("p");
-        int currentPage = 1;
-        if (currentPageString != null) {
-            try {
-                currentPage = new Integer(currentPageString);
-            } catch (NumberFormatException e) {
-                currentPage = 1;
-            }
-        }
-
-        if (currentPage < 1) {
-            currentPage = 1;
-        }
-
-        if (currentPage > pages) {
-            currentPage = pages;
-        }
-
-        int fromIndex = (currentPage - 1) * ListPosts.POSTS_PER_PAGE;
-        int toIndex = Math.min(fromIndex + ListPosts.POSTS_PER_PAGE, posts.size());
-
-        List<Post> pagePosts = posts.subList(fromIndex, toIndex);
-        List<Object> pageList = new ArrayList<>();
+        PostsPresentationBean postsPresentation = new PostsPresentationBean(category.getPostsSet());
+        int currentPage = postsPresentation.currentPage(req.getParameter("p"));
+        HashMap<String, Object> pagination = postsPresentation.paginate(page, currentPage, POSTS_PER_PAGE);
         
-        for (int i = 0; i < pages; i++) {
-            HashMap<String, Object> info = new HashMap<String, Object>();
-            info.put("url", page.getAddress() + "?p=" + (i + 1));
-            info.put("number", (i + 1));
-            pageList.add(info);
-        }
-        
-        pagination.put("pages", pageList);
-        pagination.put("current", currentPage);
-        pagination.put("posts", pagePosts);
-
-        local.put("posts", posts);
+        local.put("posts", postsPresentation.getVisiblePosts());
         local.put("pagination", pagination);
 
-        global.put("posts", posts);
+        global.put("posts", postsPresentation.getVisiblePosts());
         global.put("pagination", pagination);
     }
 
--- src/main/java/org/fenixedu/bennu/cms/domain/ListPosts.java
+++ src/main/java/org/fenixedu/bennu/cms/domain/ListPosts.java
@@ -1,10 +1,6 @@
 package org.fenixedu.bennu.cms.domain;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashMap;
-import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -18,70 +14,17 @@ public class ListPosts extends ListPosts_Base {
 
     public static final int POSTS_PER_PAGE = 5;
 
-    public ListPosts() {
-        super();
-    }
-
     @Override
     public void handle(Page page, HttpServletRequest req, TemplateContext local, TemplateContext global) {
-        List<Post> posts = new ArrayList<>();
-
-        for (Post p : page.getSite().getPostSet()) {
-            if (p.getComponentSet().size() == 0) {
-                posts.add(p);
-            }
-        }
-
-        Collections.sort(posts, new Comparator<Post>() {
-
-            @Override
-            public int compare(Post o1, Post o2) {
-                return o1.getCreationDate().compareTo(o2.getCreationDate());
-            }
-        });
-
-        int pages = (posts.size() / POSTS_PER_PAGE) + 1;
-        HashMap<String, Object> pagination = new HashMap<>();
-
-        String currentPageString = req.getParameter("p");
-        int currentPage = 1;
-        if (currentPageString != null) {
-            try {
-                currentPage = new Integer(currentPageString);
-            } catch (NumberFormatException e) {
-                currentPage = 1;
-            }
-        }
-
-        if (currentPage < 1) {
-            currentPage = 1;
-        }
-
-        if (currentPage > pages) {
-            currentPage = pages;
-        }
-
-        int fromIndex = (currentPage - 1) * POSTS_PER_PAGE;
-        int toIndex = Math.min(fromIndex + POSTS_PER_PAGE, posts.size());
-
-        List<Post> pagePosts = posts.subList(fromIndex, toIndex);
-        List<Object> pageList = new ArrayList<>();
+        PostsPresentationBean postsPresentation = new PostsPresentationBean(page.getSite().getPostSet());
+        int currentPage = postsPresentation.currentPage(req.getParameter("p"));
+        HashMap<String, Object> pagination = postsPresentation.paginate(page, currentPage, POSTS_PER_PAGE);
         
-        for (int i = 0; i < pages; i++) {
-            HashMap<String, Object> info = new HashMap<String, Object>();
-            info.put("url", page.getAddress() + "?p=" + (i + 1));
-            info.put("number", (i + 1));
-            pageList.add(info);
-        }
-        
-        pagination.put("pages", pageList);
-        pagination.put("current", currentPage);
-        pagination.put("posts", pagePosts);
-
-        local.put("posts", posts);
+        local.put("posts", postsPresentation.getVisiblePosts());
         local.put("pagination", pagination);
 
-        global.put("posts", posts);
+        global.put("posts", postsPresentation.getVisiblePosts());
         global.put("pagination", pagination);
     }
+
 }
