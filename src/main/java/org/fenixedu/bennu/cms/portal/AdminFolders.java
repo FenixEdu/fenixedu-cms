a397f3a02cf8f05c2feb2c2241494ee202c2365a
João Carvalho <joao.pedro.carvalho@tecnico.ulisboa.pt>
2014-09-10 14:19:39 +0100

    Added custom resolvers to folders

--- src/main/java/org/fenixedu/bennu/cms/portal/AdminFolders.java
+++ src/main/java/org/fenixedu/bennu/cms/portal/AdminFolders.java
@@ -2,17 +2,26 @@ package org.fenixedu.bennu.cms.portal;
 
 import static pt.ist.fenixframework.FenixFramework.atomic;
 
+import javax.script.ScriptException;
+
 import org.fenixedu.bennu.cms.domain.CMSFolder;
+import org.fenixedu.bennu.cms.domain.CMSFolder.FolderResolver;
 import org.fenixedu.bennu.core.domain.Bennu;
+import org.fenixedu.bennu.core.domain.NashornStrategy;
 import org.fenixedu.bennu.portal.domain.PortalConfiguration;
 import org.fenixedu.bennu.spring.portal.SpringFunctionality;
 import org.fenixedu.commons.i18n.LocalizedString;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.view.RedirectView;
+
+import com.google.common.base.Strings;
 
 @SpringFunctionality(app = AdminSites.class, title = "application.admin-folders.title", accessGroup = "#managers")
 @RequestMapping("/cms/folders")
@@ -30,6 +39,41 @@ public class AdminFolders {
             new CMSFolder(PortalConfiguration.getInstance().getMenu(), path, description);
         });
         return new RedirectView("/cms/folders", true);
+    }
+
+    @RequestMapping(value = "/resolver/{folder}", method = RequestMethod.GET)
+    public String folderResolver(Model model, @PathVariable CMSFolder folder) {
+        model.addAttribute("folder", folder);
+        return "folderResolver";
+    }
+
+    @RequestMapping(value = "/resolver/{folder}", method = RequestMethod.PUT)
+    public ResponseEntity<?> saveFolderResolver(@PathVariable CMSFolder folder, @RequestBody String code) {
+        try {
+            atomic(() -> {
+                if (Strings.isNullOrEmpty(code)) {
+                    folder.setResolver(null);
+                } else {
+                    folder.setResolver(new NashornStrategy<>(FolderResolver.class, code));
+                }
+                return null;
+            });
+        } catch (Exception e) {
+            Throwable original = unwrap(e);
+            return new ResponseEntity<>(original.getClass().getName() + ": " + original.getMessage(),
+                    HttpStatus.PRECONDITION_FAILED);
+        }
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    private Throwable unwrap(Throwable e) {
+        while (e.getCause() != null) {
+            if (e instanceof ScriptException) {
+                return e;
+            }
+            e = e.getCause();
+        }
+        return e;
     }
 
     @RequestMapping("/delete/{folder}")
---
+++ src/main/webapp/WEB-INF/folderResolver.jsp
@@ -0,0 +1,57 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="joda" uri="http://www.joda.org/joda/time/tags" %>
+<%@ taglib uri="http://www.springframework.org/tags" prefix="spring" %>
+
+<h1>
+	Resolver - ${folder.functionality.title.content}
+</h1>
+
+<p>
+	<a href="${pageContext.request.contextPath}/cms/folders" class="btn btn-default">« <spring:message code="action.back"/></a>
+	<button class="btn btn-success" id="saveBtn"><spring:message code="action.save" /></button>
+</p>
+
+<p class="alert alert-danger" id="error" style="display: none">
+</p>
+
+<p>
+	<pre id="editor">${folder.resolver.code}</pre>
+</p>
+
+<script src="${pageContext.request.contextPath}/static/js/ace/ace.js" type="text/javascript" charset="utf-8"></script>
+
+<style>
+	#editor {
+		height: 400px;
+	}
+</style>
+
+<script>
+    var editor = ace.edit("editor");
+
+    var submit = function() {
+      $("#error").hide();
+      $("#saveBtn").attr('disabled', true);
+      $("#saveBtn").html('<spring:message code="action.saving"/>');
+      $.ajax('${pageContext.request.contextPath}/cms/folders/resolver/${folder.externalId}', 
+             { data: editor.getValue(), type: 'PUT' }).always(function () {
+                $("#saveBtn").attr('disabled', false); $("#saveBtn").html('<spring:message code="action.save" />');
+             }).error(function (data) {
+				$("#error").show(); $("#error").html(data.responseText);
+             });
+    }
+
+    editor.setFontSize(13);
+    editor.setTheme("ace/theme/clouds");
+    editor.getSession().setMode("ace/mode/javascript");
+    editor.setHighlightActiveLine(false);
+    editor.setShowPrintMargin(false);
+    editor.commands.addCommand({
+        name: 'save',
+        bindKey: {win: 'Ctrl-S',  mac: 'Command-S'},
+        exec: submit
+    });
+
+
+    $('#saveBtn').on('click', submit);
+</script>
