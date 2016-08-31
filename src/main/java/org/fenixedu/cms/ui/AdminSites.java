68dbd56adcce9a0f822f882f3449d08635113a14
Pedro Santos <pedro.san7os@gmail.com>
2016-08-31 11:01:45 +0100

    Updated license headers

--- src/main/java/org/fenixedu/cms/ui/AdminSites.java
+++ src/main/java/org/fenixedu/cms/ui/AdminSites.java
@@ -1,18 +1,18 @@
 /**
  * Copyright © 2014 Instituto Superior Técnico
- * <p>
+ *
  * This file is part of FenixEdu CMS.
- * <p>
+ *
  * FenixEdu CMS is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * <p>
+ *
  * FenixEdu CMS is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
- * <p>
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with FenixEdu CMS.  If not, see <http://www.gnu.org/licenses/>.
  */
